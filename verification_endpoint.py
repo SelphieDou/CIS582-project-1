2f8e3954a4d1eddbfb6b772297bdb604fa8cf7b7
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-07-12 18:27:35 +0800

    v1
    original file

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, jsonify
 from flask_restful import Api
 import json
@@ -11,33 +12,10 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
 	content = request.get_json(silent=True)
-	sig = content['sig']
-	payload = content['payload']
-	platform = payload['platform']
-	message = json.dumps(payload)
-	pk = payload['pk']
-	result = False
 
 	#Check if signature is valid
-	if platform == 'Ethereum':
-		eth_encoded_msg = message
-		eth_sig = sig
-		eth_pk = pk
-		if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig) == eth_pk:
-			print( "Eth sig verifies!" )
-			result = True #Should only be true if signature validates
-
-
-	elif platform == 'Algorand':
-		algo_sig = sig
-		algo_pk = pk
-		alog_encoded_msg = message
-		if algosdk.util.verify_bytes(alog_encoded_msg,algo_sig,algo_pk):
-			print( "Algo sig verifies!" )
-			result = True #Should only be true if signature validates
-
-	
-	return jsonify(False)
+	result = True #Should only be true if signature validates
+	return jsonify(result)
 
 if __name__ == '__main__':
 	app.run(port='5002')
