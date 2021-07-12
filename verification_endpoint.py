14a89c01b7e1f4eb6a1453976daf0b33581f03cf
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-07-12 18:02:36 +0800

    v12

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -37,7 +37,6 @@ def verify():
 			result = True #Should only be true if signature validates
 
 	
-	return jsonify(True)
+	return jsonify(result)
 
-if __name__ == '__main__':
-	app.run(port='5002')
+
