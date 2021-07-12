b336e4a5e79d211b3d88c7337c9c7ab6a384469e
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-07-12 17:58:49 +0800

    v9

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -8,23 +8,6 @@ app = Flask(__name__)
 api = Api(app)
 app.url_map.strict_slashes = False
 
-
-print("start!")
-sample = {'sig': '0x3718eb506f445ecd1d6921532c30af84e89f2faefb17fc8117b75c4570134b4967a0ae85772a8d7e73217a32306016845625927835818d395f0f65d25716356c1c', 
- 'payload': 
-   {'message': 'Ethereum test message', 
-	'pk': '0x9d012d5a7168851Dc995cAC0dd810f201E1Ca8AF', 
-	'platform': 'Ethereum'}}
-sig = sample['sig']
-payload = sample['payload']
-message = json.dumps(payload)
-platform = payload['platform']
-print(sig, platform, message)
-print(type(sig))
-print(type(platform))
-print(type(message))
-
-
 @app.route('/verify', methods=['GET','POST'])
 def verify():
 	content = request.get_json(silent=True)
