3a4dc40eea93a09e4e355def41a19cd81f78e6cb
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-07-12 17:36:57 +0800

    v3

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 api = Api(app)
 app.url_map.strict_slashes = False
 
+'''
+print("start!")
+sample = {'sig': '0x3718eb506f445ecd1d6921532c30af84e89f2faefb17fc8117b75c4570134b4967a0ae85772a8d7e73217a32306016845625927835818d395f0f65d25716356c1c', 
+ 'payload': 
+   {'message': 'Ethereum test message', 
+	'pk': '0x9d012d5a7168851Dc995cAC0dd810f201E1Ca8AF', 
+	'platform': 'Ethereum'}}
+sig = sample['sig']
+payload = sample['payload']
+message = json.dumps(payload)
+platform = payload['platform']
+print(sig, platform, message)
+print(type(sig))
+print(type(platform))
+print(type(message))
+'''
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
@@ -25,7 +41,9 @@ def verify():
 		eth_pk = pk
 		if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig) == eth_pk:
 			print( "Eth sig verifies!" )
-			result = True #Should only be true if signature validates
+			#result = True #Should only be true if signature validates
+			return jsonify(result= True, user_message = message)
+
 
 	elif platform == 'Algorand':
 		algo_sig = sig
@@ -33,12 +51,12 @@ def verify():
 		alog_encoded_msg = message
 		if algosdk.util.verify_bytes(alog_encoded_msg,algo_sig,algo_pk):
 			print( "Algo sig verifies!" )
-			result = True #Should only be true if signature validates
+			#result = True #Should only be true if signature validates
+			return jsonify(result= True, user_message = message)
 
 	else:
-		result = False
-	
-	return jsonify(result)
+		
+		return jsonify(result = False, user_message = 'undefined')
 
 #verify()
 
